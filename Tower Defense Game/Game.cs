96e6a7b9d5a6838e4b6be23ec8e11c3d3b121186
Elena Medina <ElenaMedina@quickenloans.com>
2018-07-01 22:24:39 -0400

    Create files for game classes

---
+++ Tower Defense Game/Game.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TreehouseDefense
+{
+    class Game
+    {
+        public static void Main(string[] args)
+        {
+            Map map = new Map(8,5);
+            
+            int area = map.Width * map.Height;
+        }
+    }
+}
